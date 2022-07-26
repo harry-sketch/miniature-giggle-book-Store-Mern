ada7bb31a26d48fba5ddeb46bf3d227acdafc392
harry-sketch <harshbisht90@gmail.com>
2022-07-26 17:21:18 +0530

    add: delete api

--- client/src/components/pages/Books/BookComp/BookComp.jsx
+++ client/src/components/pages/Books/BookComp/BookComp.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import BookOnHover from "./BookOnHover/BookOnHover";
 
-const BookComp = ({ title, src, price, desc, id }) => {
+const BookComp = ({ title, src, price, desc, handleDelete }) => {
   return (
     <div className="text-white md:w-[19rem] md:min-h-fit border-2 border-custom-3 rounded-2xl p-2 group relative ">
-      <BookOnHover id={id} />
+      <BookOnHover handleDelete={handleDelete} />
       <div className="rounded-2xl">
         <img
           src={src}
--- client/src/components/pages/Books/BookComp/BookOnHover/BookOnHover.jsx
+++ client/src/components/pages/Books/BookComp/BookOnHover/BookOnHover.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 // Assets
-import { RiEditBoxLine } from "react-icons/ri";
 import { MdOutlineDelete } from "react-icons/md";
 
-const BookOnHover = ({ id }) => {
-  const navigate = useNavigate();
-
+const BookOnHover = ({ handleDelete }) => {
   return (
     <div className="absolute inset-0 hidden group-hover:block bg-black rounded-2xl transition-all duration-300 opacity-90">
       <div className="absolute z-50 flex items-center justify-center flex-col w-full p-4  mb-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <button className="font-bold text-2xl capitalize text-red-500 cursor-pointer border-2 border-red-500  flex items-center py-2 w-full justify-center hover:bg-red-400 hover:text-white hover:border-red-400">
+        <button
+          className="font-bold text-2xl capitalize text-red-500 cursor-pointer border-2 border-red-500  flex items-center py-2 w-full justify-center hover:bg-red-400 hover:text-white hover:border-red-400"
+          onClick={handleDelete}
+        >
           <MdOutlineDelete className="mr-2" />
           Delete
         </button>
